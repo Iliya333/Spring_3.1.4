56d9c6179cff5520bd57eefc14df87d85289f63a
Iliya333 <69747853+Iliya333@users.noreply.github.com>
2021-05-16 22:52:41 +0500

    Commit

--- src/main/java/com/Spring314Application.java
+++ src/main/java/com/Spring314Application.java
@@ -32,12 +32,12 @@ public class Spring314Application {
 		httpHeaders.add("Cookie", cookies);
 
 		ResponseEntity<String> entityPostUser = restTemplate.exchange(url, HttpMethod.POST,
-				new HttpEntity<>(new User(3L, "James", "Brown", (byte)3), httpHeaders),
+				new HttpEntity<>(new User(3L, "James", "Brown", (byte)10), httpHeaders),
 				String.class);
 		System.out.println(entityPostUser.getBody());
 
 		ResponseEntity<String> entityPutUser = restTemplate.exchange(url, HttpMethod.PUT,
-				new HttpEntity<>(new User(3L,"Thomas","Shelby", (byte)3), httpHeaders),
+				new HttpEntity<>(new User(3L,"Thomas","Shelby", (byte)10), httpHeaders),
 				String.class);
 		System.out.println(entityPutUser.getBody());
 
